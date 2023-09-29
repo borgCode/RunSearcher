5abcc093565de906a3a6d75b67d2ed05003d661c
hyper <123260045+KieranCode94@users.noreply.github.com>
2023-09-29 11:22:22 +0200

    demons souls search fixed

--- src/main/java/com/example/runsearcher/Controller.java
+++ src/main/java/com/example/runsearcher/Controller.java
@@ -3,11 +3,14 @@ package com.example.runsearcher;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import com.opencsv.exceptions.CsvException;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -21,6 +24,23 @@ import java.util.List;
 public class Controller {
 
     @FXML private TextField searchBox;
+    @FXML private CheckBox demonsSoulsCheckBox;
+    @FXML private CheckBox darkSoulsCheckBox;
+    @FXML private CheckBox darkSoulsTwoCheckBox;
+    @FXML private CheckBox darkSoulsThreeCheckBox;
+    @FXML private CheckBox bloodborneCheckBox;
+    @FXML private CheckBox sekiroCheckBox;
+    @FXML private CheckBox eldenRingCheckBox;
+    @FXML private CheckBox residentEvilCheckBox;
+    @FXML private CheckBox crashCheckBox;
+    @FXML private CheckBox dishonoredCheckBox;
+    @FXML private CheckBox zeldaCheckBox;
+    @FXML private CheckBox cupheadCheckBox;
+    @FXML private CheckBox hollowKnightCheckBox;
+    @FXML private CheckBox hadesCheckBox;
+    @FXML private CheckBox celesteCheckBox;
+    @FXML private CheckBox blasphemousCheckBox;
+    @FXML private CheckBox multipleCheckBox;
     @FXML private TableView<Run> tableView;
     @FXML private TableColumn<Run, String> runnerColumn;
     @FXML private TableColumn<Run, String> runColumn;
@@ -34,6 +54,7 @@ public class Controller {
         filterRuns();
 
     }
+
 
     public ObservableList<Run> getRuns() {
         ObservableList<Run> runs = FXCollections.observableArrayList();
@@ -60,6 +81,27 @@ public class Controller {
 
         FilteredList<Run> filteredData = new FilteredList<>(getRuns(), b -> true);
 
+
+        demonsSoulsCheckBox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean t1) {
+                filteredData.setPredicate(run -> {
+                    if (demonsSoulsCheckBox.isSelected() == false) {
+                        return true;
+                    }
+                    String filter = "demons souls";
+                    if (run.getRunName().toLowerCase()
+                            .replaceAll("’", "").replaceAll("'", "")
+                            .contains(filter)) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+
+                });
+            }
+        });
+
         searchBox.textProperty().addListener((observable, oldValue, newValue) -> {
             filteredData.setPredicate(run -> {
                 if (newValue == null || newValue.isEmpty()) {
@@ -67,11 +109,17 @@ public class Controller {
                 }
 
                 String[] values = newValue.toLowerCase().split(" ");
-                List<String> wordList = Arrays.asList(values);
+                List<String> wordList = Arrays.stream(values)
+                        .map(s -> s.replaceAll("’", "").replaceAll("'", ""))
+                        .toList();
 
-                if (wordList.stream().allMatch(Arrays.stream(run.getRunner().toLowerCase().split(" ")).toList()::contains)) {
+                if (wordList.stream().allMatch(Arrays.stream(run.getRunner().toLowerCase()
+                        .replaceAll("’", "").replaceAll("'", "")
+                        .split(" ")).toList()::contains)) {
                     return true;
-                } else if (wordList.stream().allMatch(Arrays.stream(run.getRunName().toLowerCase().split(" ")).toList()::contains)) {
+                } else if (wordList.stream().allMatch(Arrays.stream(run.getRunName().toLowerCase()
+                        .replaceAll("’", "").replaceAll("'", "")
+                        .split(" ")).toList()::contains)) {
                     return true;
                 } else {
                     return false;
@@ -80,12 +128,16 @@ public class Controller {
             });
         });
 
+
         SortedList<Run> sortedData = new SortedList<>(filteredData);
         sortedData.comparatorProperty().bind(tableView.comparatorProperty());
 
         tableView.setItems(sortedData);
 
     }
+
+    public void change
+
 
 
 }
